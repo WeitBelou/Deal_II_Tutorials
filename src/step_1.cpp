d97617b35b3cdc0bf9f7f3e493488c8207a87fb0
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2016-07-18 00:02:41 +0300

    Add comments and perform some refactoring

--- src/step_1.cpp
+++ src/step_1.cpp
@@ -1,63 +1,65 @@
+/**
+ * @file simple grid examples
+ */
+
 #include <deal.II/grid/tria.h>
-#include <deal.II/grid/tria_accessor.h>
-#include <deal.II/grid/tria_iterator.h>
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/manifold_lib.h>
 #include <deal.II/grid/grid_out.h>
-#include <iostream>
 #include <fstream>
-#include <cmath>
 using namespace dealii;
 void first_grid ()
 {
     Triangulation<2> triangulation;
     GridGenerator::hyper_cube (triangulation);
     triangulation.refine_global (4);
-    std::ofstream out ("grid-1.eps");
-    GridOut grid_out;
-    grid_out.write_eps (triangulation, out);
-    std::cout << "Grid written to grid-1.eps" << std::endl;
+    std::ofstream out ("simpleRectGrid.eps");
+    GridOut gridOut;
+    gridOut.write_eps (triangulation, out);
+    std::cout << "Grid written to simpleRectGrid.eps" << std::endl;
 }
 void second_grid ()
 {
+    const Point<2> center(1,0);
+    const double R = 1.0;
+    const double r = 0.5;
+    const int numOfCells = 10;
+
+    //Manifold define before triangulation in order to
+    const SphericalManifold<2> manifoldDescription(center);
+
     Triangulation<2> triangulation;
-    const Point<2> center (1,0);
-    const double inner_radius = 0.5,
-                     outer_radius = 1.0;
-    GridGenerator::hyper_shell (triangulation,
-                                center, inner_radius, outer_radius,
-                                10);
+
+    GridGenerator::hyper_shell (triangulation, center, r, R, numOfCells);
+
     triangulation.set_all_manifold_ids(0);
-    const SphericalManifold<2> manifold_description(center);
-    triangulation.set_manifold (0, manifold_description);
-    for (unsigned int step=0; step<5; ++step)
+    triangulation.set_manifold (0, manifoldDescription);
+
+    //Refine grid in five steps towards the inner circle of the domain
+    for (size_t i = 0; i < 5; ++i)
     {
-        Triangulation<2>::active_cell_iterator
-                cell = triangulation.begin_active(),
-                endc = triangulation.end();
-        for (; cell!=endc; ++cell)
+        //iterate through the cells
+        for (auto cell : triangulation.active_cell_iterators())
         {
-            for (unsigned int v=0;
-                 v < GeometryInfo<2>::vertices_per_cell;
-                 ++v)
+            //Check all vertices in every cell to mark it
+            for (size_t v = 0; v < GeometryInfo<2>::vertices_per_cell; ++v)
             {
-                const double distance_from_center
-                                     = center.distance (cell->vertex(v));
-                if (std::fabs(distance_from_center - inner_radius) < 1e-10)
-                {
+                const double d = center.distance(cell->vertex(v));
+                if (std::fabs(d - r) < 1e-10) {
                     cell->set_refine_flag ();
                     break;
                 }
             }
         }
+        //Let all marked cells do refinement
         triangulation.execute_coarsening_and_refinement ();
     }
-    std::ofstream out ("grid-2.eps");
-    GridOut grid_out;
-    grid_out.write_eps (triangulation, out);
-    std::cout << "Grid written to grid-2.eps" << std::endl;
-    triangulation.set_manifold (0);
+    std::ofstream file ("circularGrid.eps");
+    GridOut gridOut;
+    gridOut.write_eps(triangulation, file);
+    std::cout << "Grid written to circularGrid.eps" << std::endl;
 }
+
 int main ()
 {
     first_grid ();
